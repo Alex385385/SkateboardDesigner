19e72662b1a4f6cdc02d61f1968fffe87ae6b102
Alejandro Millan <alejandro_millan@ymail.com>
2020-03-04 00:27:40 -0800

    Initial commit

---
+++ src/TaskA.java
@@ -0,0 +1,35 @@
+//Alejandro Millan
+//CS2450
+//Assignment 2
+//3-4-20
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class TaskA extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        VBox vbox1 = new VBox();
+
+        Scene scene = new Scene(vbox1, 400, 400);
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
