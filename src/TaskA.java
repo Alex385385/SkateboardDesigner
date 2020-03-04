a6a1b280df9dfc8f2480f89e932994c0891fde3c
Alejandro Millan <alejandro_millan@ymail.com>
2020-03-04 01:21:13 -0800

    Added Listviews

--- src/TaskA.java
+++ src/TaskA.java
@@ -10,6 +10,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
@@ -17,12 +18,58 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class TaskA extends Application {
+    double price = 0.0;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        VBox vbox1 = new VBox();
+        Label test = new Label();
+        ListView<String> decks = new ListView<>();
+        decks.setPrefSize(200, 100);
+        decks.getItems().addAll(
+                "The Master Thrasher $60", "The Dictator $45", "The Street King $50");
 
-        Scene scene = new Scene(vbox1, 400, 400);
+        ListView<String> trucks = new ListView<>();
+        trucks.setPrefSize(200, 100);
+        trucks.getItems().addAll(
+                "7.75-inch axle $35", "8-inch axle $40", "8.5-inch axle $45");
+
+        ListView<String> wheels = new ListView<>();
+        wheels.setPrefSize(200, 100);
+        wheels.getItems().addAll(
+                "51 mm $20", "55 mm $22", "58 mm $24", "61 mm $28");
+
+        decks.getSelectionModel().selectedItemProperty().addListener(event -> {
+            int index = decks.getSelectionModel().getSelectedIndex();
+
+            if(index == 0) {
+                price = 60.0;
+            }
+            else if(index == 1) {
+                price = 45.0;
+            }
+            else if(index == 2) {
+                price = 50.0;
+            }
+
+            test.setText(String.format("$%f", price));
+        });
+
+        trucks.getSelectionModel().selectedItemProperty().addListener(event -> {
+            int index = trucks.getSelectionModel().getSelectedIndex();
+
+            test.setText("index");
+        });
+
+        wheels.getSelectionModel().selectedItemProperty().addListener(event -> {
+            int index = wheels.getSelectionModel().getSelectedIndex();
+
+            test.setText("index");
+        });
+
+        HBox hbox1 = new HBox(10, decks, trucks, wheels);
+        VBox vbox1 = new VBox(10, hbox1, test);
+
+        Scene scene = new Scene(vbox1, 500, 450);
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(scene);
         primaryStage.show();
