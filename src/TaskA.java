5fe8149c407076061c662922e4db72e175d4acd7
Alejandro Millan <alejandro_millan@ymail.com>
2020-03-07 21:18:02 -0800

    Added to calculate the total when making selection

--- src/TaskA.java
+++ src/TaskA.java
@@ -8,66 +8,126 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class TaskA extends Application {
-    double price = 0.0;
+    double subtotal = 0.0;
+    double salesTax = 0.0;
+    double total = 0.0;
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Label test = new Label();
+        Label subtotalLBL = new Label();
+        Label salesTaxLBL = new Label();
+        Label totalLBL = new Label();
+        Label decksLBL = new Label("Decks");
+        Label wheelsLBL = new Label("Wheels");
+        Label trucksLBL = new Label("Trucks");
+        Button getPrice = new Button("Calculate Price");
+
         ListView<String> decks = new ListView<>();
-        decks.setPrefSize(200, 100);
+        decks.setPrefSize(200, 110);
         decks.getItems().addAll(
                 "The Master Thrasher $60", "The Dictator $45", "The Street King $50");
 
         ListView<String> trucks = new ListView<>();
-        trucks.setPrefSize(200, 100);
+        trucks.setPrefSize(200, 110);
         trucks.getItems().addAll(
                 "7.75-inch axle $35", "8-inch axle $40", "8.5-inch axle $45");
 
         ListView<String> wheels = new ListView<>();
-        wheels.setPrefSize(200, 100);
+        wheels.setPrefSize(200, 110);
         wheels.getItems().addAll(
                 "51 mm $20", "55 mm $22", "58 mm $24", "61 mm $28");
 
-        decks.getSelectionModel().selectedItemProperty().addListener(event -> {
+        CheckBox gripTape = new CheckBox("Grip Tape: $10");
+        CheckBox bearings = new CheckBox("Bearings: $30");
+        CheckBox riserPads = new CheckBox("Riser pads: $2");
+        CheckBox nutNbolts = new CheckBox("Nuts & bolts kit: $3");
+
+        getPrice.setOnAction(event -> {
             int index = decks.getSelectionModel().getSelectedIndex();
+            int index2 = trucks.getSelectionModel().getSelectedIndex();
+            int index3 = wheels.getSelectionModel().getSelectedIndex();
+
+            if(index == -1 || index2 == -1 || index3 == -1) {
+                System.out.println("Erros");
+            }
 
             if(index == 0) {
-                price = 60.0;
+                subtotal = 60.0;
             }
             else if(index == 1) {
-                price = 45.0;
+                subtotal = 45.0;
             }
             else if(index == 2) {
-                price = 50.0;
+                subtotal = 50.0;
             }
 
-            test.setText(String.format("$%f", price));
+            if(index2 == 0) {
+                subtotal += 35.00;
+            }
+            else if(index2 == 1) {
+                subtotal += 40.00;
+            }
+            else if(index2 == 2) {
+                subtotal += 45.00;
+            }
+
+            if(index3 == 0) {
+                subtotal += 20.00;
+            }
+            else if(index3 == 1) {
+                subtotal += 22.00;
+            }
+            else if(index3 == 2) {
+                subtotal += 24.00;
+            }
+            else if(index3 == 3) {
+                subtotal += 28.00;
+            }
+
+            if(gripTape.isSelected()){
+                subtotal += 5.00;
+            }
+
+            if(bearings.isSelected()){
+                subtotal += 5.00;
+            }
+
+            if(riserPads.isSelected()){
+                subtotal += 5.00;
+            }
+
+            if(nutNbolts.isSelected()){
+                subtotal += 5.00;
+            }
+
+            salesTax = subtotal * 0.07;
+            total = subtotal + salesTax;
+
+            subtotalLBL.setText(String.format("Subtotal: $%.2f", subtotal));
+            salesTaxLBL.setText(String.format("Sales Tax: $%.2f", salesTax));
+            totalLBL.setText(String.format("Total: $%.2f", total));
         });
 
-        trucks.getSelectionModel().selectedItemProperty().addListener(event -> {
-            int index = trucks.getSelectionModel().getSelectedIndex();
+        VBox vDeck = new VBox(5, decksLBL, decks);
+        VBox vTrucks = new VBox(5, trucksLBL, trucks);
+        VBox vWheels = new VBox(5, wheelsLBL, wheels);
 
-            test.setText("index");
-        });
+        HBox hbox1 = new HBox(10, vDeck, vTrucks, vWheels);
+        hbox1.setAlignment(Pos.CENTER);
 
-        wheels.getSelectionModel().selectedItemProperty().addListener(event -> {
-            int index = wheels.getSelectionModel().getSelectedIndex();
+        VBox options = new VBox(10, gripTape, bearings, riserPads, nutNbolts);
+        options.setAlignment(Pos.CENTER);
 
-            test.setText("index");
-        });
-
-        HBox hbox1 = new HBox(10, decks, trucks, wheels);
-        VBox vbox1 = new VBox(10, hbox1, test);
+        VBox vbox1 = new VBox(10, hbox1, options, getPrice, subtotalLBL, salesTaxLBL, totalLBL);
+        vbox1.setAlignment(Pos.CENTER);
 
         Scene scene = new Scene(vbox1, 500, 450);
         primaryStage.setTitle("Hello World");
